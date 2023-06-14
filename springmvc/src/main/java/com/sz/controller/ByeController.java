bd77c69228193720c9e208831ebfc03d1f4d50e1
Superhero-wang2 <2934334989@qq.com>
2023-06-14 21:47:25 +0800

    qwe1

--- springmvc/src/main/java/com/sz/controller/ByeController.java
+++ springmvc/src/main/java/com/sz/controller/ByeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ByeController {
-    @RequestMapping("/bye")
+    @RequestMapping("/bye ")
     public String bye(Model model){
         model.addAttribute("model","哈哈1");
         return "bye";
